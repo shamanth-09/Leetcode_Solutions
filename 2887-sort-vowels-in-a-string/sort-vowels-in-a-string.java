61dfeb5b95608eda0d63ee2fed75e9b1b048d656
shamanth-09 <93651765+shamanth-09@users.noreply.github.com>
2023-11-14 09:37:33 +0530

    Time: 55 ms (49.51%) | Memory: 54.4 MB (5.12%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public String sortVowels(String s) {
       String vow="aeiouAEIOU";
-      Map<Character,Integer> mp=new HashMap<>();
+    //   Map<Character,Integer> mp=new HashMap<>();
       ArrayList<Character> li=new ArrayList<>();
       ArrayList<Integer> li1=new ArrayList<>();
       for(int i=0;i<s.length();i++){
@@ -16,7 +16,7 @@ class Solution {
       for(int i=0;i<li.size();i++){
           str.setCharAt(li1.get(i),li.get(i));
       }
-              System.out.print(li);
+            //   System.out.print(li);
       return str.toString();  
     }
 }
